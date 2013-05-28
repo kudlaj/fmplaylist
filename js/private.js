5ed25da493c01a1067e9e56e8bc6896345fd2638
Jürgen Kudla <kudlaj@gmail.com>
2013-05-28 11:44:23 +0300

    Create private.js

---
+++ js/private.js
@@ -0,0 +1,11 @@
+var PrivateKeys = (function () {
+  var privateKeys = {
+			JAMENDO_KEY : "eac55c93",
+			SOUNDCLOUD_KEY : "f21005ad936093c154cae9fa962d0905",
+			FMA_KEY : "QQXLPGKVP01F6N7N",
+			LASTFM_KEY : "1b7b94e72e1194b9944a33668f1838ff",
+			ECHONEST_KEY : "BYCX1QJMJFB50HJPA"
+			};
+
+	return privateKeys;
+}());
