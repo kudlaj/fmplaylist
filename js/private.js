d115fb518dd3fc00ded9aa0b8e97eee63721d016
Jürgen Kudla <kudlaj@gmail.com>
2014-09-19 13:57:55 +0200

    Create private.js

---
+++ js/private.js
@@ -0,0 +1,11 @@
+var PrivateKeys = (function () {
+  var privateKeys = {
+            JAMENDO_KEY : "xxxxxx",
+            SOUNDCLOUD_KEY : "xxxx",
+            FMA_KEY : "xxxx",
+            LASTFM_KEY : "xxxxx",
+            ECHONEST_KEY : "xxxxx"
+            };
+
+    return privateKeys;
+}());
